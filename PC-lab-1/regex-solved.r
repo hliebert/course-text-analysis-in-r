621872bc408a2178ca550c57e5386464b9f9b855
Helge Liebert <helge.liebert@gmail.com>
2022-02-16 17:12:46 +0100

    Solutions and fixes after the first day.

--- PC-lab-1/regex-solved.r
+++ PC-lab-1/regex-solved.r
@@ -3,7 +3,7 @@
 ## Description: 
 ## Author: Helge Liebert
 ## Created: Fr Jan 21 14:29:15 2022
-## Last-Updated: Mi Feb 16 13:04:09 2022
+## Last-Updated: Mi Feb 16 13:03:56 2022
 ################################################################################
 
 library("gutenbergr")
@@ -157,16 +157,28 @@ countries[str_detect(countries, "land")]
 
 
 ## TASK: Find all capitals in countries beginning with a vowel and not ending with "land".
-## ...
+capitals[grepl("^[AEIOU].*", countries) & !grepl("land$", countries)]
 
 ## TASK: Find all countries that contain exactly two words in the title and swap them.
-## ...
+grep("([A-Z][a-z]+) ([A-Z][a-z]+)", countries, value = TRUE)
+gsub("([A-Z][a-z]+) ([A-Z][a-z]+)", "\\2 \\1", countries)
+
+## accounting for Bosnia and Herzegovina
+grep("([A-Z][a-z]+)( (& )*)([A-Z][a-z]+)", countries, value = TRUE)
+gsub("([A-Z][a-z]+)( | & )([A-Z][a-z]+)", "\\3\\2\\1", countries)
 
 
 #================================ More exercises ===============================
 
 ## Project Gutenberg offers lots of free classic literature
 gutenberg_works(languages = "en")
+
+## gutenberg_works(author == "London, Jack", title == "Call of the Wild")
+## gutenberg_works(author == "Twain, Mark", title == "Adventures of Huckleberry Finn")
+## gutenberg_works(author == "Goethe, Johann Wolfgang von", title == "Faust")
+## gutenberg_works(author == "Dostoyevsky, Fyodor", title == "Crime and Punishment")
+## gutenberg_works(author == "Conrad, Joseph", title == "Heart of Darkness")
+## gutenberg_works(author == "Defoe, Daniel", title == "The Life and Adventures of Robinson Crusoe")
 
 
 #=============================== Robinson Crusoe ===============================
@@ -178,14 +190,42 @@ robinson <- robinson$text
 robinson
 
 ## TASK: How many lines in the book mention Friday?
-## ...
+sum(grepl("Friday", robinson))
 
 ## TASK: How many lines in the book mention Friday or goats?
-## ...
+sum(grepl("Friday|goat", robinson))
 
 ## TASK: On which line does he first mention finding another man's footprint on the beach?
 ##       What does the paragraph say?
-## ...
+grep("foot", robinson)
+grep("print", robinson)
+
+robinson[grep("foot", robinson)]
+robinson[grep("print", robinson)]
+
+# lines which mention foot and print
+grepl("foot", robinson) & grepl("print", robinson)
+robinson[grepl("foot", robinson) & grepl("print", robinson)]
+
+## Alternative ways of doing the same
+
+## capturing groups and logical OR
+grep("(print.*foot)|(foot.*print)", robinson)
+
+## Positive lookahead assertion, non-consuming
+grep("(?=.*print)(?=.*foot)", robinson, perl = TRUE)
+
+# these are the lines we are looking for
+robinson[grep("(?=.*print)(?=.*foot)", robinson, perl = TRUE)]
+
+# alternative
+robinson[grep("man.s.*foot", robinson)]
+robinson[grep("man\\Ss.*foot", robinson)]
+
+line <- grep("print of a man.s naked foot", robinson)
+line
+
+robinson[(line - 1):(line + 19)]
 
 
 #==================================== Faust ====================================
@@ -205,3 +245,51 @@ faust.de <- faust.de$text
 
 ## Hint: Places and actors are in full caps.
 ## Hint: Faust asks devil who he is.
+## Hint: Devil speaks in riddles.
+## Hint: "part of power", "spirit which denies"
+## Hint: "gutes will und böses schafft", "geist der verneint"
+
+
+faust[grep("will.*evil", faust, ignore.case = TRUE)]
+faust[grep("work.*good", faust, ignore.case = TRUE)]
+faust[1000:1200]
+
+## none of these are informative
+faust[grep("will", faust, ignore.case = TRUE)]
+faust[grep("good", faust, ignore.case = TRUE)]
+faust[grep("eternal", faust, ignore.case = TRUE)]
+faust[grep("power", faust, ignore.case = TRUE)]
+
+## if you remember parts of the citation correctly, this is it
+faust[grep("who.*you", faust, ignore.case = TRUE)]
+faust[grep("who.*thou", faust, ignore.case = TRUE)]
+faust[grep("spirit.*denies", faust, ignore.case = TRUE)]
+
+## Alternatively, identify the chapter in the study and the speakers (Faust and Mephistopheles)
+grep("MEPHISTOPHELES", faust)
+grep("STUDY", faust)
+grep("study", faust, ignore.case = TRUE)
+
+begin.chapter <- grep("STUDY", faust)[1]
+devil.speaks <- grep("MEPHISTOPHELES", faust)
+faust.speaks <- grep("FAUST", faust)
+other.speaks <- grep("[A-Z][A-Z]+", faust)
+
+devil.speaks.instudy <- devil.speaks[devil.speaks > begin.chapter]
+faust.speaks.instudy <- faust.speaks[faust.speaks > begin.chapter]
+
+devil.speaks.instudy <- devil.speaks.instudy[devil.speaks.instudy > min(faust.speaks.instudy)]
+faust.speaks.instudy <- faust.speaks.instudy[faust.speaks.instudy > min(devil.speaks.instudy)]
+
+devil.speaks.instudy <- devil.speaks.instudy[1:10]
+faust.speaks.instudy <- faust.speaks.instudy[1:10]
+
+devil.talks <- unlist(Map(`:`, devil.speaks.instudy, faust.speaks.instudy - 1))
+
+faust[devil.talks]
+faust[1996:2021]
+
+## German, easier if you remember the citation
+grep("geist.*verneint", faust.de, value = TRUE, ignore.case = TRUE)
+line <- grep("geist.*verneint", faust.de, ignore.case = TRUE)
+faust.de[(line - 10):(line + 7)]
